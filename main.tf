77da8c7329f57843b16ef8a33f654b398e6f7c0c
BirGer05 <gergobirinyi@gmail.com>
2023-05-17 20:29:09 +0200

    így e

--- main.tf
+++ main.tf
@@ -12,11 +12,6 @@ module "read" {
 
 module "write" {
   source = "./modules/write"
-  answer_1 = var.answer_1
-  answer_2 = var.answer_2
-  answer_3 = var.answer_3
-  answer_4 = var.answer_4
-  answer_5 = var.answer_5
 }
 
 module "data" {
