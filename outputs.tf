f3a877907ce1bf6b1e7298fba2f741c5d9ee5761
BirGer05 <gergobirinyi@gmail.com>
2023-05-17 20:25:29 +0200

    gf

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,23 @@ output "output_goal" {
 output "file_id" {
   value = module.data.fiel_id
 }
+
+output "answer_1" {
+  value = var.answer_1
+}
+
+output "answer_2" {
+  value = var.answer_2
+}
+
+output "answer_3" {
+  value = var.answer_3
+}
+
+output "answer_4" {
+  value = var.answer_4
+}
+
+output "answer_5" {
+  value = var.answer_5
+}
