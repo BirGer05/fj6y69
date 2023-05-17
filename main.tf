383b7cbbc9f01fea29cb0f5b53cdf2a8be53a871
BirGer05 <gergobirinyi@gmail.com>
2023-05-17 20:32:42 +0200

    last

--- main.tf
+++ main.tf
@@ -12,6 +12,11 @@ module "read" {
 
 module "write" {
   source = "./modules/write"
+  answer_1 = answers.answer_1
+  answer_2 = answers.answer_2
+  answer_3 = answers.answer_3
+  answer_4 = answers.answer_4
+  answer_5 = answers.answer_5
 }
 
 module "data" {
