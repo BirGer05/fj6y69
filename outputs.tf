9a4c79304d75a9c8f913290fdc1bcd892ef5177a
BirGer05 <gergobirinyi@gmail.com>
2023-05-17 20:26:28 +0200

    eafg

--- outputs.tf
+++ outputs.tf
@@ -5,23 +5,3 @@ output "output_goal" {
 output "file_id" {
   value = module.data.fiel_id
 }
-
-output "answer_1" {
-  value = var.answer_1
-}
-
-output "answer_2" {
-  value = var.answer_2
-}
-
-output "answer_3" {
-  value = var.answer_3
-}
-
-output "answer_4" {
-  value = var.answer_4
-}
-
-output "answer_5" {
-  value = var.answer_5
-}
